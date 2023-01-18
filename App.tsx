8affd2b0943a23d7fc24abc9d417b10b575dc6e0
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2023-01-18 11:10:35 -0400

    Travail de l'authentification

--- App.tsx
+++ App.tsx
@@ -1,29 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { Amplify, Auth } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
-
-import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+// @ts-ignore
+import { Authenticator, SignIn, SignUp } from 'aws-amplify-react-native'
 import HomeScreen from './screens/HomeScreen';
+import { DefaultScreenProps } from './types/screen';
 Amplify.configure(awsconfig);
 
 const App = () => {
+
   return (
-    <><View style={styles.container}>
-      <Authenticator
-        hideSignUp={true}
-      >
-        {({ signOut, user }) => (
-          <>
-            <HomeScreen user={user} onSignOut={signOut} />
-            <StatusBar style="auto" />
-          </>
-        )}
-      </Authenticator>
-    </View>
-    </>
+    <Authenticator>
+      <HomeScreen />
+    </Authenticator>
   );
 }
 
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,22 +1,18 @@
 import React, { useEffect } from 'react'
 import { Button, Text, View } from 'react-native'
 import { getAllAdmins } from '../classes/Administrator'
-import { DefaultScreenProps } from '../types/screen'
 
-const HomeScreen: React.FunctionComponent<DefaultScreenProps> = ({ onSignOut, user }) => {
+const HomeScreen = () => {
+    /* 
     useEffect(() => {
         const admins = getAllAdmins();
         admins.then(res => console.log(res))
-    }, [])
-
+    }, []) */
 
     return (
         <View>
             <Text>Hello World</Text>
-            <Button
-                title='Deconnexion'
-                onPress={onSignOut}
-            />
+            <Text>Hello World</Text>
         </View>
     )
 }
