209a70b7ddc995a6e7ddb9652cf85ebb18916bd6
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2023-01-18 11:41:34 -0400

    Implementation de l'authentification

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify, I18n} from 'aws-amplify';
 import awsconfig from './src/aws-exports';
-// @ts-ignore
-import { Authenticator, SignIn, SignUp } from 'aws-amplify-react-native'
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
 import HomeScreen from './screens/HomeScreen';
 import { DefaultScreenProps } from './types/screen';
+import { Button } from 'react-native';
 Amplify.configure(awsconfig);
+
+import { translations } from '@aws-amplify/ui';
+I18n.putVocabularies(translations);
+I18n.setLanguage('fr');
+
+I18n.putVocabularies({
+  fr: {
+    'Sign In': 'Se connecter',
+    'Sign Up': "S'inscrire",
+    'Forgot Password?': "Mot de passe oublié ?",
+    'Enter your Username':"Entrer votre nom d'utilisateur",
+    'Enter your Password':"Entrer votre mot de passe",
+    'Reset Password' : 'Reinitialiser votre mot de passe'
+  },
+});
+
+
+function SignOutButton() {
+  const { signOut } = useAuthenticator();
+  return <Button title="Sign Out" onPress={signOut} />;
+}
 
 const App = () => {
 
   return (
-    <Authenticator>
-      <HomeScreen />
-    </Authenticator>
+    <Authenticator.Provider>
+      <Authenticator
+        components={{
+          SignIn: (props) => (
+            <Authenticator.SignIn {...props} hideSignUp />
+          ),
+        }}
+      >
+        <View style={styles.container}>
+          <HomeScreen />
+          <SignOutButton />
+        </View>
+
+      </Authenticator>
+    </Authenticator.Provider>
   );
 }
 
